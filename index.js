2f1799ce91f4b02f3fcb49a03f02e8b9cee04c0b
witty-collection-3981 <yisroel96@gmail.com>
2018-09-28 06:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string === "toLowerCase"){
+  if (string.toLowerCase === string){
     return ("I can't hear you!")
   }
-}
+
